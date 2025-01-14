dhanyarmathews/opentelemetry-collector-contrib

commit 667e81e28b7da3e50b1338a3becf4aef7aa231e4

[exporter/doris] Send json lines to doris rather than json array (#36912)
<!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
Ex. Adding a feature - Explain what this achieves.-->
#### Description 
Send json lines to doris rather than json array.

<!-- Issue number (e.g. #1234) or full URL to issue, if applicable. -->
#### Link to tracking issue
Fixes #36896

<!--Describe what testing was performed and which tests were added.-->
#### Testing
unit test

<!--Describe the documentation added.-->
#### Documentation

<!--Please delete paragraphs that you did not use before submitting.-->

exporter/dorisexporter/exporter_common.go
@@ -7,6 +7,7 @@ import (
 	"bytes"
 	"context"
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -81,7 +82,7 @@ func streamLoadRequest(ctx context.Context, cfg *Config, table string, data []by
 
 	req.Header.Set("format", "json")
 	req.Header.Set("Expect", "100-continue")
-	req.Header.Set("strip_outer_array", "true")
+	req.Header.Set("read_json_by_line", "true")
 	if cfg.ClientConfig.Timeout != 0 {
 		req.Header.Set("timeout", fmt.Sprintf("%d", cfg.ClientConfig.Timeout/time.Second))
 	}
@@ -117,4 +118,20 @@ func createAndUseDatabase(ctx context.Context, conn *sql.DB, cfg *Config) error 
 	}
 	_, err = conn.ExecContext(ctx, "USE "+cfg.Database)
 	return err
+}
+
+type metric interface {
+	dMetricGauge | dMetricSum | dMetricHistogram | dMetricExponentialHistogram | dMetricSummary
+}
+
+func toJsonLines[T dLog | dTrace | metric](data []*T) ([]byte, error) {
+	buf := &bytes.Buffer{}
+	enc := json.NewEncoder(buf)
+	for _, d := range data {
+		err := enc.Encode(d)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return buf.Bytes(), nil
 }

exporter/dorisexporter/exporter_logs.go
@@ -122,7 +122,7 @@ func (e *logsExporter) pushLogData(ctx context.Context, ld plog.Logs) error {
 }
 
 func (e *logsExporter) pushLogDataInternal(ctx context.Context, logs []*dLog) error {
-	marshal, err := json.Marshal(logs)
+	marshal, err := toJsonLines(logs)
 	if err != nil {
 		return err
 	}

exporter/dorisexporter/exporter_traces.go
@@ -180,7 +180,7 @@ func (e *tracesExporter) pushTraceData(ctx context.Context, td ptrace.Traces) er
 }
 
 func (e *tracesExporter) pushTraceDataInternal(ctx context.Context, traces []*dTrace) error {
-	marshal, err := json.Marshal(traces)
+	marshal, err := toJsonLines(traces)
 	if err != nil {
 		return err
 	}

exporter/dorisexporter/metrics_exponential_histogram.go
@@ -5,7 +5,6 @@ package dorisexporter // import "github.com/open-telemetry/opentelemetry-collect
 
 import (
 	_ "embed"
-	"encoding/json"
 	"fmt"
 
 	"go.opentelemetry.io/collector/pdata/pmetric"
@@ -118,5 +117,5 @@ func (m *metricModelExponentialHistogram) size() int {
 }
 
 func (m *metricModelExponentialHistogram) bytes() ([]byte, error) {
-	return json.Marshal(m.data)
+	return toJsonLines(m.data)
 }

exporter/dorisexporter/metrics_gauge.go
@@ -5,7 +5,6 @@ package dorisexporter // import "github.com/open-telemetry/opentelemetry-collect
 
 import (
 	_ "embed"
-	"encoding/json"
 	"fmt"
 
 	"go.opentelemetry.io/collector/pdata/pmetric"
@@ -84,5 +83,5 @@ func (m *metricModelGauge) size() int {
 }
 
 func (m *metricModelGauge) bytes() ([]byte, error) {
-	return json.Marshal(m.data)
+	return toJsonLines(m.data)
 }

exporter/dorisexporter/metrics_histogram.go
@@ -5,7 +5,6 @@ package dorisexporter // import "github.com/open-telemetry/opentelemetry-collect
 
 import (
 	_ "embed"
-	"encoding/json"
 	"fmt"
 
 	"go.opentelemetry.io/collector/pdata/pmetric"
@@ -108,5 +107,5 @@ func (m *metricModelHistogram) size() int {
 }
 
 func (m *metricModelHistogram) bytes() ([]byte, error) {
-	return json.Marshal(m.data)
+	return toJsonLines(m.data)
 }

exporter/dorisexporter/metrics_sum.go
@@ -5,7 +5,6 @@ package dorisexporter // import "github.com/open-telemetry/opentelemetry-collect
 
 import (
 	_ "embed"
-	"encoding/json"
 	"fmt"
 
 	"go.opentelemetry.io/collector/pdata/pmetric"
@@ -88,5 +87,5 @@ func (m *metricModelSum) size() int {
 }
 
 func (m *metricModelSum) bytes() ([]byte, error) {
-	return json.Marshal(m.data)
+	return toJsonLines(m.data)
 }

exporter/dorisexporter/metrics_summary.go
@@ -5,7 +5,6 @@ package dorisexporter // import "github.com/open-telemetry/opentelemetry-collect
 
 import (
 	_ "embed"
-	"encoding/json"
 	"fmt"
 
 	"go.opentelemetry.io/collector/pdata/pmetric"
@@ -89,5 +88,5 @@ func (m *metricModelSummary) size() int {
 }
 
 func (m *metricModelSummary) bytes() ([]byte, error) {
-	return json.Marshal(m.data)
+	return toJsonLines(m.data)
 }

commit 0c89ab537e81180e2455878511c31eea1f223aa6

[chore] fix CI (#37177)
Fix lint issue caused by
https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/36912

This reverts commit
https://github.com/open-telemetry/opentelemetry-collector-contrib/commit/61f93ff7f14d3368bed981c447cbbc52d3c26d86.

@adrielp @niwoerner I can't explain why, but it seems that after
https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/36838
the extension integration tests never complete. You can see in
https://github.com/open-telemetry/opentelemetry-collector-contrib/actions/workflows/build-and-test.yml?query=branch%3Amain
that we haven't got an integration test suite to pass since merging the
new receiver. Reverting to unblock CI.

Makefile
@@ -42,7 +42,10 @@ EXPORTER_MODS_1 := $(shell find ./exporter/[d-i]* $(FIND_MOD_ARGS) -exec $(TO_MO
 EXPORTER_MODS_2 := $(shell find ./exporter/[k-o]* $(FIND_MOD_ARGS) -exec $(TO_MOD_DIR) )
 EXPORTER_MODS_3 := $(shell find ./exporter/[p-z]* $(FIND_MOD_ARGS) -exec $(TO_MOD_DIR) )
 EXPORTER_MODS := $(EXPORTER_MODS_0) $(EXPORTER_MODS_1) $(EXPORTER_MODS_2) $(EXPORTER_MODS_3)
-EXTENSION_MODS := $(shell find ./extension/* $(FIND_MOD_ARGS) -exec $(TO_MOD_DIR) )
+EXTENSION_MODS_0 := $(shell find ./extension/[a-d]* $(FIND_MOD_ARGS) -exec $(TO_MOD_DIR) )
+EXTENSION_MODS_1 := $(shell find ./extension/[e]* $(FIND_MOD_ARGS) -exec $(TO_MOD_DIR) )
+EXTENSION_MODS_2 := $(shell find ./extension/[f-z]* $(FIND_MOD_ARGS) -exec $(TO_MOD_DIR) )
+EXTENSION_MODS := $(EXTENSION_MODS_0) $(EXTENSION_MODS_1) $(EXTENSION_MODS_2)
 CONNECTOR_MODS := $(shell find ./connector/* $(FIND_MOD_ARGS) -exec $(TO_MOD_DIR) )
 INTERNAL_MODS := $(shell find ./internal/* $(FIND_MOD_ARGS) -exec $(TO_MOD_DIR) )
 PKG_MODS := $(shell find ./pkg/* $(FIND_MOD_ARGS) -exec $(TO_MOD_DIR) )
@@ -78,7 +81,9 @@ all-groups:
 	@echo "\nexporter-1: $(EXPORTER_MODS_1)"
 	@echo "\nexporter-2: $(EXPORTER_MODS_2)"
 	@echo "\nexporter-3: $(EXPORTER_MODS_3)"
-	@echo "\nextension: $(EXTENSION_MODS)"
+	@echo "\nextension-0: $(EXTENSION_MODS_0)"
+	@echo "\nextension-1: $(EXTENSION_MODS_1)"
+	@echo "\nextension-2: $(EXTENSION_MODS_2)"
 	@echo "\nconnector: $(CONNECTOR_MODS)"
 	@echo "\ninternal: $(INTERNAL_MODS)"
 	@echo "\npkg: $(PKG_MODS)"
@@ -244,6 +249,15 @@ for-exporter-3-target: $(EXPORTER_MODS_3)
 
 .PHONY: for-extension-target
 for-extension-target: $(EXTENSION_MODS)
+
+.PHONY: for-extension-0-target
+for-extension-0-target: $(EXTENSION_MODS_0)
+
+.PHONY: for-extension-1-target
+for-extension-1-target: $(EXTENSION_MODS_1)
+
+.PHONY: for-extension-2-target
+for-extension-2-target: $(EXTENSION_MODS_2)
 
 .PHONY: for-connector-target
 for-connector-target: $(CONNECTOR_MODS)

exporter/dorisexporter/exporter_common.go
@@ -124,7 +124,7 @@ type metric interface {
 	dMetricGauge | dMetricSum | dMetricHistogram | dMetricExponentialHistogram | dMetricSummary
 }
 
-func toJsonLines[T dLog | dTrace | metric](data []*T) ([]byte, error) {
+func toJSONLines[T dLog | dTrace | metric](data []*T) ([]byte, error) {
 	buf := &bytes.Buffer{}
 	enc := json.NewEncoder(buf)
 	for _, d := range data {

exporter/dorisexporter/exporter_logs.go
@@ -122,7 +122,7 @@ func (e *logsExporter) pushLogData(ctx context.Context, ld plog.Logs) error {
 }
 
 func (e *logsExporter) pushLogDataInternal(ctx context.Context, logs []*dLog) error {
-	marshal, err := toJsonLines(logs)
+	marshal, err := toJSONLines(logs)
 	if err != nil {
 		return err
 	}

exporter/dorisexporter/exporter_traces.go
@@ -180,7 +180,7 @@ func (e *tracesExporter) pushTraceData(ctx context.Context, td ptrace.Traces) er
 }
 
 func (e *tracesExporter) pushTraceDataInternal(ctx context.Context, traces []*dTrace) error {
-	marshal, err := toJsonLines(traces)
+	marshal, err := toJSONLines(traces)
 	if err != nil {
 		return err
 	}

exporter/dorisexporter/metrics_exponential_histogram.go
@@ -117,5 +117,5 @@ func (m *metricModelExponentialHistogram) size() int {
 }
 
 func (m *metricModelExponentialHistogram) bytes() ([]byte, error) {
-	return toJsonLines(m.data)
+	return toJSONLines(m.data)
 }

exporter/dorisexporter/metrics_gauge.go
@@ -83,5 +83,5 @@ func (m *metricModelGauge) size() int {
 }
 
 func (m *metricModelGauge) bytes() ([]byte, error) {
-	return toJsonLines(m.data)
+	return toJSONLines(m.data)
 }

exporter/dorisexporter/metrics_histogram.go
@@ -107,5 +107,5 @@ func (m *metricModelHistogram) size() int {
 }
 
 func (m *metricModelHistogram) bytes() ([]byte, error) {
-	return toJsonLines(m.data)
+	return toJSONLines(m.data)
 }

exporter/dorisexporter/metrics_sum.go
@@ -87,5 +87,5 @@ func (m *metricModelSum) size() int {
 }
 
 func (m *metricModelSum) bytes() ([]byte, error) {
-	return toJsonLines(m.data)
+	return toJSONLines(m.data)
 }

exporter/dorisexporter/metrics_summary.go
@@ -88,5 +88,5 @@ func (m *metricModelSummary) size() int {
 }
 
 func (m *metricModelSummary) bytes() ([]byte, error) {
-	return toJsonLines(m.data)
+	return toJSONLines(m.data)
 }

.github/workflows/build-and-test.yml
@@ -351,7 +351,9 @@ jobs:
           - exporter-1
           - exporter-2
           - exporter-3
-          - extension
+          - extension-0
+          - extension-1
+          - extension-2
           - connector
           - internal
           - pkg
@@ -380,7 +382,7 @@ jobs:
 
   integration-tests:
     if: ${{ github.actor != 'dependabot[bot]' && always() }}
-    runs-on: ubuntu-24.04
+    runs-on: ubuntu-latest
     needs: [setup-environment, integration-tests-matrix]
     steps:
       - name: Print result

exporter/dorisexporter/exporter_common_test.go
@@ -63,7 +63,7 @@ func findRandomPort() (int, error) {
 }
 
 func TestToJsonLines(t *testing.T) {
-	logs, err := toJsonLines([]*dLog{
+	logs, err := toJSONLines([]*dLog{
 		{}, {},
 	})
 	require.NoError(t, err)