dhanyarmathews/opentelemetry-collector-contrib

commit 1da7b511b71d9bca121efdfa33f432537fca70d3

[chore] switch over githubgen to the one built with our tools (#37397)
<!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
Ex. Adding a feature - Explain what this achieves.-->
#### Description
Change the githubgen we use to the one defined in
opentelemetry-go-build-tools

<!-- Issue number (e.g. #1234) or full URL to issue, if applicable. -->
#### Link to tracking issue
See
https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/37294

Makefile
@@ -310,17 +310,13 @@ generate: install-tools
 	PATH="$$PWD/.tools:$$PATH" $(MAKE) for-all CMD="$(GOCMD) generate ./..."
 	$(MAKE) gofmt
 
-.PHONY: githubgen-install
-githubgen-install:
-	cd cmd/githubgen && $(GOCMD) install .
-
 .PHONY: gengithub
-gengithub: githubgen-install
-	githubgen
+gengithub: $(GITHUBGEN)
+	$(GITHUBGEN)
 
 .PHONY: gendistributions
-gendistributions: githubgen-install
-	githubgen distributions
+gendistributions: $(GITHUBGEN)
+	$(GITHUBGEN) distributions
 
 .PHONY: update-codeowners
 update-codeowners: gengithub generate
@@ -556,9 +552,8 @@ clean:
 	find . -type f -name 'builtunitetest.test' -delete
 
 .PHONY: generate-gh-issue-templates
-generate-gh-issue-templates:
-	cd cmd/githubgen && $(GOCMD) install .
-	githubgen issue-templates
+generate-gh-issue-templates: $(GITHUBGEN)
+	$(GITHUBGEN) issue-templates
 
 .PHONY: checks
 checks:

Makefile.Common
@@ -74,6 +74,7 @@ CHECKFILE           := $(TOOLS_BIN_DIR)/checkfile
 CROSSLINK           := $(TOOLS_BIN_DIR)/crosslink
 GOJUNIT             := $(TOOLS_BIN_DIR)/go-junit-report
 BUILDER             := $(TOOLS_BIN_DIR)/builder
+GITHUBGEN           := $(TOOLS_BIN_DIR)/githubgen
 GOFUMPT             := $(TOOLS_BIN_DIR)/gofumpt
 GOVULNCHECK         := $(TOOLS_BIN_DIR)/govulncheck
 GCI                 := $(TOOLS_BIN_DIR)/gci

internal/tools/go.mod
@@ -14,6 +14,7 @@ require (
 	go.opentelemetry.io/build-tools/checkfile v0.16.0
 	go.opentelemetry.io/build-tools/chloggen v0.16.0
 	go.opentelemetry.io/build-tools/crosslink v0.16.0
+	go.opentelemetry.io/build-tools/githubgen v0.0.0-20250120111601-b29324c2a9f5
 	go.opentelemetry.io/build-tools/issuegenerator v0.16.0
 	go.opentelemetry.io/build-tools/multimod v0.16.0
 	go.opentelemetry.io/collector/cmd/builder v0.118.1-0.20250121185328-fbefb22cc2b3
@@ -107,7 +108,8 @@ require (
 	github.com/golangci/unconvert v0.0.0-20240309020433-c5143eacb3ed // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/go-github v17.0.0+incompatible // indirect
-	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/google/go-github/v66 v66.0.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
 	github.com/gordonklaus/ineffassign v0.1.0 // indirect
 	github.com/gostaticanalysis/analysisutil v0.7.1 // indirect

internal/tools/go.sum
@@ -227,8 +227,10 @@ github.com/google/go-cmp v0.6.0 h1:ofyhxvXcZhMsU5ulbFiLKl/XBFqE1GSq7atu8tAmTRI=
 github.com/google/go-cmp v0.6.0/go.mod h1:17dUlkBOakJ0+DkrSSNjCkIjxS6bF9zb3elmeNGIjoY=
 github.com/google/go-github v17.0.0+incompatible h1:N0LgJ1j65A7kfXrZnUDaYCs/Sf4rEjNlfyDHW9dolSY=
 github.com/google/go-github v17.0.0+incompatible/go.mod h1:zLgOLi98H3fifZn+44m+umXrS52loVEgC2AApnigrVQ=
-github.com/google/go-querystring v1.0.0 h1:Xkwi/a1rcvNg1PPYe5vI8GbeBY/jrVuDX5ASuANWTrk=
-github.com/google/go-querystring v1.0.0/go.mod h1:odCYkC5MyYFN7vkCjXpyrEuKhc/BUO6wN/zVPAxq5ck=
+github.com/google/go-github/v66 v66.0.0 h1:ADJsaXj9UotwdgK8/iFZtv7MLc8E8WBl62WLd/D/9+M=
+github.com/google/go-github/v66 v66.0.0/go.mod h1:+4SO9Zkuyf8ytMj0csN1NR/5OTR+MfqPp8P8dVlcvY4=
+github.com/google/go-querystring v1.1.0 h1:AnCroh3fv4ZBgVIf1Iwtovgjaw/GiKJo8M8yD/fhyJ8=
+github.com/google/go-querystring v1.1.0/go.mod h1:Kcdr2DB4koayq7X8pmAG4sNG59So17icRSOU623lUBU=
 github.com/google/gofuzz v1.0.0/go.mod h1:dBl0BpW6vV/+mYPU4Po3pmUjxk6FQPldtuIdl/M65Eg=
 github.com/google/pprof v0.0.0-20240827171923-fa2c70bbbfe5 h1:5iH8iuqE5apketRbSFBy+X1V0o+l+8NF1avt4HWl7cA=
 github.com/google/pprof v0.0.0-20240827171923-fa2c70bbbfe5/go.mod h1:vavhavw2zAxS5dIdcRluK6cSGGPlZynqzFM8NdvU144=
@@ -572,6 +574,8 @@ go.opentelemetry.io/build-tools/chloggen v0.16.0 h1:iuotHxlcK46JJtQLdwQPsC5dcAyg
 go.opentelemetry.io/build-tools/chloggen v0.16.0/go.mod h1:Wk92v9Wsv36sXYi7hOg3ndeeLKmKBu0/kgB7wcaeqJg=
 go.opentelemetry.io/build-tools/crosslink v0.16.0 h1:7Y5QPt5TR3qpiW5bwIOnsCJdt1yjZSFDPKtjt2g2zHw=
 go.opentelemetry.io/build-tools/crosslink v0.16.0/go.mod h1:xogE6iWmt53bsDazb81dQrZw9TQ30+9hc4D8QfVG9aA=
+go.opentelemetry.io/build-tools/githubgen v0.0.0-20250120111601-b29324c2a9f5 h1:oBdYXMV8QxvU43yuBlvFE0xks8GjZxUgWZzjl5K6E0w=
+go.opentelemetry.io/build-tools/githubgen v0.0.0-20250120111601-b29324c2a9f5/go.mod h1:HV+4y/3X+rbyhhGInkBByBozPIAjQpL6MxFrHtnYSW4=
 go.opentelemetry.io/build-tools/issuegenerator v0.16.0 h1:Ka14LdI0suh63HR25gy0deuspgBThl/z95e/LIIaiRw=
 go.opentelemetry.io/build-tools/issuegenerator v0.16.0/go.mod h1:JfdlpwSKBYbwdTOdGGlCRw4vptEgnYPhApnACBps8N8=
 go.opentelemetry.io/build-tools/multimod v0.16.0 h1:o205mGH61VXebXeiIPXwxeHRCm332TZqRb8gmypI6po=

internal/tools/tools.go
@@ -22,6 +22,7 @@ import (
 	_ "go.opentelemetry.io/build-tools/checkfile"
 	_ "go.opentelemetry.io/build-tools/chloggen"
 	_ "go.opentelemetry.io/build-tools/crosslink"
+	_ "go.opentelemetry.io/build-tools/githubgen"
 	_ "go.opentelemetry.io/build-tools/issuegenerator"
 	_ "go.opentelemetry.io/build-tools/multimod"
 	_ "go.opentelemetry.io/collector/cmd/builder"

commit 787659830ec3a00691c402aaa4b74aac068d9661

[chore] Revert the switch over githubgen to the one built with our tools (#37409)
Reverts open-telemetry/opentelemetry-collector-contrib#37397.

PRs started to fail after #37397 got merged because of the codeowners
check action, because it runs `make githubgen-install` then calls the
`githubgen` tool.

I tried first fixing the CI check to run the new tool and it's giving
errors, which is already starting to affect new PRs and old ones that
get rebased:

```
/Users/douglas/code/opentelemetry-collector-contrib/.tools/githubgen
2025/01/22 11:43:55 codeowners are not members: Erog38, Frapschen, IbraheemA, JonathanWamsley, MovieStoreGuy, Mrod1598, NassimBtk, RichieSams, StefanKurek, VihasMakwana, adrielp, ag-ramachandran, akats7, alexvanboxel, ankitpa
tel96, antonblock, armstrmi, atingchen, atoulme, avadhut123pisal, bertysentry, braydonk, bryan-aguilar, carsonip, chan-tim-sumo, chrroberts-pure, codeboten, cparkins, cpheps, dashpole, dehaansa, dgoscn, edmocosta, eedorenko, 
erichsueh3, evan-bradley, fatsheep9146, fchikwekwe, gjanco, greatestusername, hughesjj, jacobmarble, jade-guiton-dd, jamesmoessis, jaronoff97, jmacd, joker-star-l, jsuereth, kasia-kujawa, khyatigandhi6, kongluoxing, liustanle
y, lquerel, mapno, mar4uk, marctc, martin-majlis-s1, mcardy, michalpristas, moh-osman3, mrsillydog, narcis96, nslaughter, pavankrish123, pcwiese, pjanotti, portertech, povilasv, psx95, pxaws, qiansheng91, rapphil, rnishtala-s
umo, rogercoll, samiura, schmikei, shabicheng, shalper2, sinkingpoint, smithclay, splunkericl, srprash, swiatekm, tamir-michaeli, tomaz-s1, wangzlei, yotamloe, zdaratom-s1
make: *** [gengithub] Error 1
```

I suggest that the new tool is introduced more gradually and is not used
in CI yet until it behaves exactly like the old one.

Makefile
@@ -310,13 +310,17 @@ generate: install-tools
 	PATH="$$PWD/.tools:$$PATH" $(MAKE) for-all CMD="$(GOCMD) generate ./..."
 	$(MAKE) gofmt
 
+.PHONY: githubgen-install
+githubgen-install:
+	cd cmd/githubgen && $(GOCMD) install .
+
 .PHONY: gengithub
-gengithub: $(GITHUBGEN)
-	$(GITHUBGEN)
+gengithub: githubgen-install
+	githubgen
 
 .PHONY: gendistributions
-gendistributions: $(GITHUBGEN)
-	$(GITHUBGEN) distributions
+gendistributions: githubgen-install
+	githubgen distributions
 
 .PHONY: update-codeowners
 update-codeowners: gengithub generate
@@ -552,8 +556,9 @@ clean:
 	find . -type f -name 'builtunitetest.test' -delete
 
 .PHONY: generate-gh-issue-templates
-generate-gh-issue-templates: $(GITHUBGEN)
-	$(GITHUBGEN) issue-templates
+generate-gh-issue-templates:
+	cd cmd/githubgen && $(GOCMD) install .
+	githubgen issue-templates
 
 .PHONY: checks
 checks:

Makefile.Common
@@ -74,7 +74,6 @@ CHECKFILE           := $(TOOLS_BIN_DIR)/checkfile
 CROSSLINK           := $(TOOLS_BIN_DIR)/crosslink
 GOJUNIT             := $(TOOLS_BIN_DIR)/go-junit-report
 BUILDER             := $(TOOLS_BIN_DIR)/builder
-GITHUBGEN           := $(TOOLS_BIN_DIR)/githubgen
 GOFUMPT             := $(TOOLS_BIN_DIR)/gofumpt
 GOVULNCHECK         := $(TOOLS_BIN_DIR)/govulncheck
 GCI                 := $(TOOLS_BIN_DIR)/gci

internal/tools/go.mod
@@ -14,7 +14,6 @@ require (
 	go.opentelemetry.io/build-tools/checkfile v0.16.0
 	go.opentelemetry.io/build-tools/chloggen v0.16.0
 	go.opentelemetry.io/build-tools/crosslink v0.16.0
-	go.opentelemetry.io/build-tools/githubgen v0.0.0-20250120111601-b29324c2a9f5
 	go.opentelemetry.io/build-tools/issuegenerator v0.16.0
 	go.opentelemetry.io/build-tools/multimod v0.16.0
 	go.opentelemetry.io/collector/cmd/builder v0.118.1-0.20250121185328-fbefb22cc2b3
@@ -108,8 +107,7 @@ require (
 	github.com/golangci/unconvert v0.0.0-20240309020433-c5143eacb3ed // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/go-github v17.0.0+incompatible // indirect
-	github.com/google/go-github/v66 v66.0.0 // indirect
-	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
 	github.com/gordonklaus/ineffassign v0.1.0 // indirect
 	github.com/gostaticanalysis/analysisutil v0.7.1 // indirect

internal/tools/go.sum
@@ -227,10 +227,8 @@ github.com/google/go-cmp v0.6.0 h1:ofyhxvXcZhMsU5ulbFiLKl/XBFqE1GSq7atu8tAmTRI=
 github.com/google/go-cmp v0.6.0/go.mod h1:17dUlkBOakJ0+DkrSSNjCkIjxS6bF9zb3elmeNGIjoY=
 github.com/google/go-github v17.0.0+incompatible h1:N0LgJ1j65A7kfXrZnUDaYCs/Sf4rEjNlfyDHW9dolSY=
 github.com/google/go-github v17.0.0+incompatible/go.mod h1:zLgOLi98H3fifZn+44m+umXrS52loVEgC2AApnigrVQ=
-github.com/google/go-github/v66 v66.0.0 h1:ADJsaXj9UotwdgK8/iFZtv7MLc8E8WBl62WLd/D/9+M=
-github.com/google/go-github/v66 v66.0.0/go.mod h1:+4SO9Zkuyf8ytMj0csN1NR/5OTR+MfqPp8P8dVlcvY4=
-github.com/google/go-querystring v1.1.0 h1:AnCroh3fv4ZBgVIf1Iwtovgjaw/GiKJo8M8yD/fhyJ8=
-github.com/google/go-querystring v1.1.0/go.mod h1:Kcdr2DB4koayq7X8pmAG4sNG59So17icRSOU623lUBU=
+github.com/google/go-querystring v1.0.0 h1:Xkwi/a1rcvNg1PPYe5vI8GbeBY/jrVuDX5ASuANWTrk=
+github.com/google/go-querystring v1.0.0/go.mod h1:odCYkC5MyYFN7vkCjXpyrEuKhc/BUO6wN/zVPAxq5ck=
 github.com/google/gofuzz v1.0.0/go.mod h1:dBl0BpW6vV/+mYPU4Po3pmUjxk6FQPldtuIdl/M65Eg=
 github.com/google/pprof v0.0.0-20240827171923-fa2c70bbbfe5 h1:5iH8iuqE5apketRbSFBy+X1V0o+l+8NF1avt4HWl7cA=
 github.com/google/pprof v0.0.0-20240827171923-fa2c70bbbfe5/go.mod h1:vavhavw2zAxS5dIdcRluK6cSGGPlZynqzFM8NdvU144=
@@ -574,8 +572,6 @@ go.opentelemetry.io/build-tools/chloggen v0.16.0 h1:iuotHxlcK46JJtQLdwQPsC5dcAyg
 go.opentelemetry.io/build-tools/chloggen v0.16.0/go.mod h1:Wk92v9Wsv36sXYi7hOg3ndeeLKmKBu0/kgB7wcaeqJg=
 go.opentelemetry.io/build-tools/crosslink v0.16.0 h1:7Y5QPt5TR3qpiW5bwIOnsCJdt1yjZSFDPKtjt2g2zHw=
 go.opentelemetry.io/build-tools/crosslink v0.16.0/go.mod h1:xogE6iWmt53bsDazb81dQrZw9TQ30+9hc4D8QfVG9aA=
-go.opentelemetry.io/build-tools/githubgen v0.0.0-20250120111601-b29324c2a9f5 h1:oBdYXMV8QxvU43yuBlvFE0xks8GjZxUgWZzjl5K6E0w=
-go.opentelemetry.io/build-tools/githubgen v0.0.0-20250120111601-b29324c2a9f5/go.mod h1:HV+4y/3X+rbyhhGInkBByBozPIAjQpL6MxFrHtnYSW4=
 go.opentelemetry.io/build-tools/issuegenerator v0.16.0 h1:Ka14LdI0suh63HR25gy0deuspgBThl/z95e/LIIaiRw=
 go.opentelemetry.io/build-tools/issuegenerator v0.16.0/go.mod h1:JfdlpwSKBYbwdTOdGGlCRw4vptEgnYPhApnACBps8N8=
 go.opentelemetry.io/build-tools/multimod v0.16.0 h1:o205mGH61VXebXeiIPXwxeHRCm332TZqRb8gmypI6po=

internal/tools/tools.go
@@ -22,7 +22,6 @@ import (
 	_ "go.opentelemetry.io/build-tools/checkfile"
 	_ "go.opentelemetry.io/build-tools/chloggen"
 	_ "go.opentelemetry.io/build-tools/crosslink"
-	_ "go.opentelemetry.io/build-tools/githubgen"
 	_ "go.opentelemetry.io/build-tools/issuegenerator"
 	_ "go.opentelemetry.io/build-tools/multimod"
 	_ "go.opentelemetry.io/collector/cmd/builder"