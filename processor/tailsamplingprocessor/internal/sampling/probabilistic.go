532df56498ae85114baffc5ec6038b481eeb0833
Dhanya Mathews <dhanyarmathews@ibm.com>
2025-11-25 14:30:54 +0530

    Syncup for migration

--- processor/tailsamplingprocessor/internal/sampling/probabilistic.go
+++ processor/tailsamplingprocessor/internal/sampling/probabilistic.go
@@ -43,7 +43,7 @@ func NewProbabilisticSampler(settings component.TelemetrySettings, hashSalt stri
 
 // Evaluate looks at the trace data and returns a corresponding SamplingDecision.
 func (s *probabilisticSampler) Evaluate(_ context.Context, traceID pcommon.TraceID, _ *TraceData) (Decision, error) {
-	s.logger.Debug("Evaluating spans in probabilistic filter")
+	s.logger.Debug("[CHECK]:Evaluating spans in probabilistic filter")
 
 	if hashTraceID(s.hashSalt, traceID[:]) <= s.threshold {
 		return Sampled, nil
